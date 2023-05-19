33b11ad5a57d688c6ab5698f682213db042ededb
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2023-05-19 16:19:07 +0530

    changes in daily comprehension service

--- db/services/comprehension_crud.js
+++ db/services/comprehension_crud.js
@@ -31,8 +31,19 @@ module.exports = {
     const result = await ComResultModel.findOne({email});
     if(result){
       const exclude_comprehensions = result.exclude_comprehensions;
-      console.log(exclude_comprehensions);
-      const excludeIds = exclude_comprehensions.map(id => new ObjectId(id));
+      const lastAttemptedOnDate = new Date(result.updatedAt);
+      const today = new Date();
+
+      // Set the time part of 'today' to 00:00:00
+      lastAttemptedOnDate.setHours(0, 0, 0, 0);
+      today.setHours(0, 0, 0, 0);
+
+      if (lastAttemptedOnDate.getTime() === today.getTime())
+        return {
+          msg: "You have already completed your daily exercise for the day. Come back tomorrow!!",
+          showDialog: true
+        };
+      const excludeIds = exclude_comprehensions.map((id) => new ObjectId(id));
       const account_level = result.account_level;
       const doc = await ComModel.aggregate([
         // Exclude comprehensions with matching IDs or levels exceeding the account level
@@ -40,25 +51,32 @@ module.exports = {
           $match: {
             $and: [
               { _id: { $nin: excludeIds } },
-              { level: { $lte: account_level } }
-            ]
-          }
+              { level: { $lte: account_level } },
+            ],
+          },
         },
-      // Randomly sort the documents
-      { $sample: { size: 1 } },
-    ]).catch((error) => console.log(error));
-    
-    if (doc) {
-   return doc[0];
-    } 
-  }
+        // Randomly sort the documents
+        { $sample: { size: 1 } },
+      ]).catch((error) => console.log(error));
 
-  const count = await ComModel.countDocuments();
+      if (doc) {
+        return doc[0];
+      }
+    }
+
+  const count = 7;
 
     // Generate a random index within the range of the document count
     const randomIndex = Math.floor(Math.random() * count);
     // Retrieve a random document using the $sample operator
     const randomDocument = await ComModel.aggregate([
+      {
+        $match: {
+         
+            level: { $eq: [1] } 
+          
+        }
+      },
       { $skip: randomIndex },
       { $sample: { size: 1 } }
     ]).catch((error) => console.log(error));
